351327067621a1b213ffd07fc4d4eea3ae6f904d
Clif Houck <me@clifhouck.com>
2013-04-09 23:48:34 -0500

    Added rough version of an 8 bit register class.

---
+++ src/utility/Register.cpp
@@ -0,0 +1,59 @@
+#include "Register.hpp"
+
+Register::
+Register(u8_byte initialData,
+        u8_byte resetData) :
+    m_data (initialData),
+    m_resetData (resetData)
+{
+}
+
+u8_byte 
+Register::
+read() const
+{
+    return m_data;
+}
+
+    void    
+write(u8_byte data, u8_byte mask)
+{
+    m_data = m_data & mask;
+}
+
+u8_byte 
+Register::
+rawRead() const
+{
+    return m_data;
+}
+
+void    
+Register::
+rawWrite(u8_byte data, u8_byte mask)
+{
+    m_data = data & mask;
+}
+    
+void 
+Register::
+reset()
+{
+    PoweredDevice::reset();
+    m_data = m_resetData;
+}
+
+void
+Register::
+powerOn() 
+{
+    PoweredDevice::powerOn();
+    m_data = m_resetData;
+}
+
+void
+Register::
+powerOff()
+{
+    PoweredDevice::powerOff();
+}
---
+++ src/utility/Register.hpp
@@ -0,0 +1,32 @@
+#ifndef REGISTER_H
+#define REGISTER_
+
+#include "DataTypes.hpp"
+
+#include "PoweredDevice.hpp"
+
+class Register : public PoweredDevice
+{
+public:
+    Register(u8_byte initialData,
+             u8_byte resetData);
+
+    // Functions for checked reads and writes.
+    u8_byte read() const;
+    void    write(u8_byte data, u8_byte mask);
+
+    // Ignores rules like read-only masks and returns the raw data found in the backing store.
+    u8_byte rawRead() const;
+    void    rawWrite(u8_byte data, u8_byte mask);
+
+    // PoweredDevice interface
+    virtual void reset();
+    virtual void powerOn();
+    virtual void powerOff();
+
+private:
+    u8_byte m_data;
+    u8_byte m_resetData;
+};
+
+#endif //REGISTER_H
